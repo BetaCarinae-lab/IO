5450c20675ec785042608071171b571d36e78942
Betacarinae <finn.d.fagan@gmail.com>
2026-05-04 17:02:34 -0600

    Fixing my basic placing and click logic

--- main.gd
+++ main.gd
@@ -5,15 +5,24 @@ var gates: Dictionary[String, PackedScene] = {
 }
 @onready var testbutton = get_node("Control/ColorRect/Button")
 @onready var buttonscript = get_node("Control")
+var holding = gates["test"].instantiate()
+var dragging = false
 
 func _ready() -> void:
 	testbutton.pressed.connect(_on_button_pressed.bind("test"))
 	print("Loaded")
-	
-func _on_button_pressed(button: String) -> void:
-	print("BUTTON CLICKED: " + button)
-	
+	print(holding)
 
+func _on_button_pressed(button: String) -> void:
+	holding = gates[button].instantiate()
+	add_child(holding)
+	dragging = true
 
 func _input(event: InputEvent) -> void:
-	pass
+	if event is InputEventMouseMotion and dragging and holding:
+		holding.position = event.position  # follow mouse
+
+	if event.is_action_released("holdanddrop") and dragging:
+		holding.position = event.position  # place at final position
+		holding = null                     # drop reference, object stays in scene
+		dragging = false
